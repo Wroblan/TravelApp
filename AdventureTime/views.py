06615a3b5f389f6903fd34d2173afee07ab06bfa
Hermar52 <hermar525252@gmail.com>
2023-06-29 18:30:15 +0200

    Merge remote-tracking branch 'origin/main_2' into main_2
    # Conflicts:
    #	.idea/workspace.xml

--- AdventureTime/views.py
+++ AdventureTime/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import View, CreateView, UpdateView, DeleteView
+from AdventureTime import models
+
 
 def hello(request):
   return HttpResponse('Hello, world!')
+
+class CountryReadView(View):
+  def get(self, request):
+      return render(request, template_name='country_read.html', context={'dane': models.Country.objects.all()})
